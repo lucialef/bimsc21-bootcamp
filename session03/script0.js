3a554080b2b49bcc876ad83a41d2392c70dfb718
lucialef <lucialevafuentes@gmail.com>
2022-01-17 16:31:21 +0100

    Updated version

---
+++ session03/script0.js
@@ -0,0 +1,54 @@
+//console.log("Hey, I am in an external file")
+
+const doc = document.documentElement
+//console.log(doc)
+
+doc.style.fontFamily = "monospace";
+doc.style.background = "black";
+doc.style.color = "white";
+
+
+//access elements from the doc
+
+const titlebar = document.documentElement.querySelector("#child-1");
+//console.log(titlebar.innerHTML);
+titlebar.innerHTML = "This is my website's titlebar";
+titlebar.style.fontSize = "20px";
+
+//add elements into the site
+let resetButton = document.createElement("button");
+titlebar.appendChild(resetButton);
+resetButton.innerHTML = "Reset Button";
+resetButton.style.float = "right";
+
+//assign an event to my button
+resetButton.addEventListener("click", doSomething);
+
+function doSomething(){
+    console.log("button has been pressed");
+}
+
+const mainText = document.documentElement.querySelector("p");
+mainText.innerHTML = ""
+
+const sidebar = document.documentElement.querySelector("#child-2");
+
+//create a form (user input) to fill the paragraph
+let input = document.createElement("input");
+
+sidebar.appendChild(input);
+
+let submitButton = document.createElement("button");
+submitButton.innerHTML = "Submit";
+sidebar.appendChild(submitButton);
+
+let val = submitButton.addEventListener('click', submitText);
+
+function submitText(){
+    let inputText = input.value;
+    console.log(inputText);
+    mainText.innerHTML += inputText + "";
+    input.value = "";
+
+};
+
